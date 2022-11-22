df8ea5914ddc46af1c5ada4e11ffd359c06477ff
mrgarrik <kosourikhinio@gmail.com>
2022-11-22 17:07:15 +0700

    scroll page with js

---
+++ 2.2_rb_cb_scroll_js_step6.py
@@ -0,0 +1,39 @@
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+import time
+import math
+def calc(x):
+  return str(math.log(abs(12*math.sin(int(x)))))
+
+try: 
+    link = "http://suninjuly.github.io/execute_script.html"
+    browser = webdriver.Chrome()
+    browser.get(link)
+    
+    x_element = browser.find_element(By.CSS_SELECTOR, "#input_value")
+    x = x_element.text
+    y = calc(x)
+    ans = browser.find_element(By.CSS_SELECTOR, "#answer")
+    browser.execute_script("return arguments[0].scrollIntoView(true);", ans)
+    input1 = browser.find_element(By.CSS_SELECTOR, "#answer")
+    input1.send_keys(y)
+    option1 = browser.find_element(By.CSS_SELECTOR, "#robotCheckbox")
+    option1.click()
+    option2 = browser.find_element(By.CSS_SELECTOR, "#robotsRule")
+    option2.click()
+    
+    # Отправляем заполненную форму
+    button = browser.find_element(By.CSS_SELECTOR, ".btn")
+    button.click()
+
+    # Проверяем, что смогли зарегистрироваться
+    # ждем загрузки страницы
+    time.sleep(1)
+
+
+
+finally:
+    # ожидание чтобы визуально оценить результаты прохождения скрипта
+    time.sleep(10)
+    # закрываем браузер после всех манипуляций
+    browser.quit()
