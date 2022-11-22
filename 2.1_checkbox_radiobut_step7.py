c54a1911cc05e00f956651a3fb5c23361d360d7e
mrgarrik <kosourikhinio@gmail.com>
2022-11-22 17:05:11 +0700

    click checkbox and radio button

---
+++ 2.1_checkbox_radiobut_step7.py
@@ -0,0 +1,39 @@
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+import time
+import math
+def calc(x):
+  return str(math.log(abs(12*math.sin(int(x)))))
+
+try: 
+    link = "http://suninjuly.github.io/get_attribute.html"
+    browser = webdriver.Chrome()
+    browser.get(link)
+    
+    x_element = browser.find_element(By.CSS_SELECTOR, ".bg-light #treasure")
+    x = x_element.get_attribute("valuex")
+    y = calc(x)
+
+    # Ваш код, который заполняет обязательные поля
+    input1 = browser.find_element(By.CSS_SELECTOR, "#answer")
+    input1.send_keys(y)
+    option1 = browser.find_element(By.CSS_SELECTOR, "#robotCheckbox")
+    option1.click()
+    option2 = browser.find_element(By.CSS_SELECTOR, "#robotsRule")
+    option2.click()
+    
+    # Отправляем заполненную форму
+    button = browser.find_element(By.CSS_SELECTOR, ".btn")
+    button.click()
+
+    # Проверяем, что смогли зарегистрироваться
+    # ждем загрузки страницы
+    time.sleep(1)
+
+
+
+finally:
+    # ожидание чтобы визуально оценить результаты прохождения скрипта
+    time.sleep(5)
+    # закрываем браузер после всех манипуляций
+    browser.quit()
